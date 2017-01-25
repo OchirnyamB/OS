5e378bc94fb98ccc7e5e735af25f8b5709c7574b
Ali Raza <aardz6@mail.missouri.edu>
2017-01-24 19:30:49 -0600

    Strings and System Programming

--- C Programming Review/strings/src/sstring.c
+++ C Programming Review/strings/src/sstring.c
@@ -1,15 +1,26 @@
 #include <string.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../include/sstring.h"
 
 bool string_valid(const char *str, const size_t length) {
 
+    if(str == NULL || length == 0) return false;
+
+    if(str[length-1] == '\0') return true;
+    
 	return false;
 }
 
 char *string_duplicate(const char *str, const size_t length) {
+
+    if(str == NULL || length == 0) return NULL;
+    else {
+        char * new_str = malloc(sizeof(char) * length);
+        return strncpy(new_str, str, length);
+    }
 
 	return NULL;
 
@@ -17,20 +28,72 @@ char *string_duplicate(const char *str, const size_t length) {
 
 bool string_equal(const char *str_a, const char *str_b, const size_t length) {
 
-	return false;
+	if (str_a == NULL || str_b == NULL || length == 0) return false;
+    
+    if(!strcmp(str_a, str_b)) return true;
+
+    return false;
 }
 
 int string_length(const char *str, const size_t length) {
 
-	return 0;
+    if(str == NULL || length == 0) return -1;
+    return strlen(str);
 }
 
 int string_tokenize(const char *str, const char *delims, const size_t str_length,char **tokens, const size_t max_token_length, const size_t requested_tokens) {
 
-	return 0;
+    if(str == NULL || delims == NULL || str_length == 0 || tokens == NULL || requested_tokens == 0 || max_token_length == 0) return 0; 
+
+    for(int i = 0; i < requested_tokens; i++){
+        if(tokens[i] == NULL) return -1; 
+    }
+    
+
+    char* src = malloc(sizeof(char)*str_length);
+    //memset(src, '\0', sizeof(src));
+    strcpy(src, str);
+
+    printf("%s", src);
+    char* token = malloc(sizeof(char)*max_token_length);
+    //token = strtok(src, delims);
+   /* int i = 0;
+    strcpy(*(tokens+i), token);
+    printf("%s", (tokens);*/
+    //i++;
+    
+    /*while(token != NULL){
+        token = strtok(NULL, delims);
+        strcpy(*(tokens+i), token);
+      //  i++;
+    }*/
+
+    /*int i =0;
+    char* token;
+    token = strtok(src, delims);
+    strcpy(*(tokens+i), token);
+    
+    while(token != NULL){
+        token = strtok(NULL, delims);
+        strcpy(*(tokens+i), token);
+        i++;
+    }
+
+    while(*tokens != NULL){
+        *(tokens+i) = (strtok(str, delims));
+        i++;
+    }*/
+    free(token);
+	return 1;
 }
 
 bool string_to_int(const char *str, int *converted_value) {
+
+    if(str == NULL || converted_value == NULL) return false;
+
+    if(atoi(str) == -1) return false;
+    
+    else *converted_value = atoi(str);
 
 	return true;
 }
--- C Programming Review/system_programming/src/sys_prog.c
+++ C Programming Review/system_programming/src/sys_prog.c
@@ -13,10 +13,49 @@
 
 bool bulk_read(const char *input_filename, void *dst, const size_t offset, const size_t dst_size) {
 
+    if(input_filename == NULL || dst == NULL || !strcmp(dst, "\n") || !strcmp(dst, "\0") || offset > dst_size || dst_size == 0) return false;
+
+    int descriptor = open(input_filename, O_RDWR);
+    if(descriptor < 0) return false;
+    
+    size_t read_data; 
+
+    if(offset == 0){
+        read_data = read(descriptor, dst, dst_size-offset);
+    } else {
+        read_data = read(descriptor, dst, offset);
+    }
+    
+    if(read_data < 0) return false;
+ 
+    if(read_data == dst_size) return true;
+
 	return false;
 }
 
 bool bulk_write(const void *src, const char *output_filename, const size_t offset, const size_t src_size) {
+
+    if(output_filename == NULL || src == NULL || !strcmp(output_filename, "\n") || !strcmp(output_filename, "\0") || !strcmp(output_filename, "") || src_size == 0) return false;
+
+    int descriptor = open(output_filename, O_WRONLY);
+    if(descriptor < 0) return false;
+
+    size_t write_data;
+
+    if(offset == 0){
+        write_data = write(descriptor, src, src_size-offset);
+    } else if(offset > src_size) {
+        write_data = write(descriptor, src, src_size);
+        //return true;
+    } else{
+        write_data = write(descriptor, src, offset);
+    }
+
+    printf("%d\n", (int)write_data);
+
+    if(write_data < 0) return false;
+
+    if(write_data == src_size) return true;
 
 	return false;
 }
